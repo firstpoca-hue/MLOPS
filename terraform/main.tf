af7e469637adfa4d2781776dd1a48d27c227a33c
Sashangy Kumar <sashangy.kumar@atos.net>
2025-11-24 11:59:32 +0530

    infra+mlops

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 module "s3" {
   source      = "./modules/s3"
   bucket_name = var.bucket_name
--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,21 +1,9 @@
 resource "aws_s3_bucket" "ml_bucket" {
-  bucket = var.bucket_name
-}
-
-resource "aws_s3_bucket_versioning" "ml_bucket_versioning" {
-  bucket = aws_s3_bucket.ml_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+  bucket        = var.bucket_name
+  force_destroy = true
 }
 
 resource "aws_s3_bucket" "pipeline_artifacts" {
-  bucket = "${var.bucket_name}-pipeline-artifacts"
-}
-
-resource "aws_s3_bucket_versioning" "pipeline_artifacts_versioning" {
-  bucket = aws_s3_bucket.pipeline_artifacts.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+  bucket        = "${var.bucket_name}-pipeline-artifacts"
+  force_destroy = true
 }
