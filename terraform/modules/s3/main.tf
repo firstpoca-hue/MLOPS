c7784f84fe5f3164493c9e299bb82360d6981f5e
Sashangy Kumar <sashangy.kumar@atos.net>
2025-11-24 12:20:17 +0530

    infra+mlops

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,9 +1,36 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "ml_bucket" {
-  bucket        = var.bucket_name
+  bucket        = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
   force_destroy = true
 }
 
 resource "aws_s3_bucket" "pipeline_artifacts" {
-  bucket        = "${var.bucket_name}-pipeline-artifacts"
+  bucket        = "${var.bucket_name}-pipeline-${random_id.bucket_suffix.hex}"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "ml_bucket_versioning" {
+  bucket = aws_s3_bucket.ml_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "pipeline_artifacts_versioning" {
+  bucket = aws_s3_bucket.pipeline_artifacts.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
