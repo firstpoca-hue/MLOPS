9207becc8bce90b51a03fb3b736c7208fa7491de
Sashangy Kumar <sashangy.kumar@atos.net>
2025-11-28 08:35:21 +0530

    Force Lambda zip update via Terraform hash

--- terraform/lambda_package.tf
+++ terraform/lambda_package.tf
@@ -2,6 +2,7 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
   output_path = "${path.module}/lambda_function.zip"
+  source_content_hash = filebase64sha256("${path.module}/../lambda_function.py")
   
   source {
     content = file("${path.module}/../lambda_function.py")
